f28aee7aed7c230d945c49f43627cefa66d6ecd4
KinderRiven <1339764596@qq.com>
2021-06-17 11:30:06 +0800

    rdma demo simple push

--- hello_rdma.cc
+++ hello_rdma.cc
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-06-17 10:56:52
- * @LastEditTime: 2021-06-17 11:29:19
+ * @LastEditTime: 2021-06-17 11:29:58
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /rdma_demo/hello_rdma.cc
@@ -29,17 +29,19 @@ int main(int argc, char** argv)
     printf("Hello RDMA!\n");
 
     struct ibv_device** dev_list = NULL;
+    struct ibv_device* dev;
     dev_list = ibv_get_device_list(NULL);
     if ((*dev_list) == NULL) {
         printf("Failed to get ib device list!\n");
         exit(1);
     } else {
         printf("Get ib device list! [0x%x]\n", *dev_list);
+        dev = *dev_list;
     }
 
     IBRes _ib;
     memset(&_ib, 0, sizeof(_ib));
-    _ib.ctx = ibv_open_device(*dev_list);
+    _ib.ctx = ibv_open_device(*dev);
     if (_ib.ctx == NULL) {
         printf("Failed to open ib device!\n");
         exit(2);
