48d999e52f622aa90ad29a753dc1243f376ce5e0
KinderRiven <1339764596@qq.com>
2021-06-17 11:05:54 +0800

    rdma demo simple push

---
+++ gitpush.sh
@@ -0,0 +1,13 @@
+###
+ # @Author: your name
+ # @Date: 2021-06-17 11:05:29
+ # @LastEditTime: 2021-06-17 11:05:49
+ # @LastEditors: Please set LastEditors
+ # @Description: In User Settings Edit
+ # @FilePath: /rdma_demo/gitpush.sh
+### 
+
+
+git add *
+git commit -m "rdma demo simple push"
+git push
--- hello_rdma.cc
+++ hello_rdma.cc
@@ -1,8 +1,32 @@
+/*
+ * @Author: your name
+ * @Date: 2021-06-17 10:56:52
+ * @LastEditTime: 2021-06-17 11:05:21
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /rdma_demo/hello_rdma.cc
+ */
+#include <infiniband/verbs.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv)
+struct IBRes {
+    struct ibv_context* ctx;
+    struct ibv_pd* pd;
+    struct ibv_mr* mr;
+    struct ibv_cq* cq;
+    struct ibv_qp** qp;
+    struct ibv_srq* srq;
+    struct ibv_port_attr port_attr;
+    struct ibv_device_attr dev_attr;
+
+    int num_qps;
+    char* ib_buf;
+    size_t ib_buf_size;
+};
+
+int main(int argc, char** argv)
 {
-    printf("Hello World!\n");
+    printf("Hello RDMA!\n");
     return 0;
 }
