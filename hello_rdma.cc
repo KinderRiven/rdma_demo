42a0660a814d01f85356e08e7f92942e76e1e911
KinderRiven <1339764596@qq.com>
2021-06-19 20:05:40 +0800

    rdma demo simple push

--- hello_rdma.cc
+++ hello_rdma.cc
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-06-17 10:56:52
- * @LastEditTime: 2021-06-19 20:04:57
+ * @LastEditTime: 2021-06-19 20:05:38
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /rdma_demo/hello_rdma.cc
@@ -113,6 +113,6 @@ int main(int argc, char** argv)
 {
     rdma_context_t _ctx;
     memset(&_ctx, 0, sizeof(_ctx));
-    open_device(_ctx);
+    open_device(&_ctx);
     return 0;
 }
