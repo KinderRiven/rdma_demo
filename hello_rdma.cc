dc8dbc3b4592853cf8da04a06e4808545284d8c3
KinderRiven <1339764596@qq.com>
2021-06-20 12:09:03 +0800

    rdma demo simple push

--- hello_rdma.cc
+++ hello_rdma.cc
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-06-17 10:56:52
- * @LastEditTime: 2021-06-20 12:06:46
+ * @LastEditTime: 2021-06-20 12:08:38
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /rdma_demo/hello_rdma.cc
@@ -177,8 +177,26 @@ static void create_qpair(rdma_context_t* context)
     }
 
     struct ibv_qp_init_attr qp_init_attr;
+    /*
+        struct ibv_qp_init_attr {
+            void		        *qp_context;
+            struct ibv_cq	    *send_cq;
+            struct ibv_cq	    *recv_cq;
+            struct ibv_srq	    *srq;
+            struct ibv_qp_cap	cap;
+            enum ibv_qp_type	qp_type;
+            int			        sq_sig_all;
+        }; 
+        struct ibv_qp_cap {
+            uint32_t		max_send_wr;
+            uint32_t		max_recv_wr;
+            uint32_t		max_send_sge;
+            uint32_t		max_recv_sge;
+            uint32_t		max_inline_data;
+        }
+    */
     memset(&qp_init_attr, 0, sizeof(qp_init_attr));
-    qp_init_attr.qp_context = context->ctx;
+    // qp_init_attr.qp_context = context->ctx;
     qp_init_attr.send_cq = context->cq;
     qp_init_attr.recv_cq = context->cq;
     qp_init_attr.srq = context->srq;
