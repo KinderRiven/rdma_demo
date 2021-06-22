45381482cd3310eab22f4d0d54a6fcd3414c29f3
KinderRiven <1339764596@qq.com>
2021-06-22 10:39:51 +0800

    rdma demo simple push

--- server.cc
+++ server.cc
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-06-17 10:56:52
- * @LastEditTime: 2021-06-22 10:31:01
+ * @LastEditTime: 2021-06-22 10:39:46
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /rdma_demo/hello_rdma.cc
@@ -274,13 +274,13 @@ static int modify_qp_to_rtr(struct ibv_qp* qp, uint32_t remote_qpn, uint16_t dli
     attr.ah_attr.src_path_bits = 0;
     attr.ah_attr.port_num = 1;
 
-    // attr.ah_attr.is_global = 1;
-    // attr.ah_attr.port_num = 1;
-    // memcpy(&attr.ah_attr.grh.dgid, dgid, 16);
-    // attr.ah_attr.grh.flow_label = 0;
-    // attr.ah_attr.grh.hop_limit = 1;
-    // attr.ah_attr.grh.sgid_index = 2;
-    // attr.ah_attr.grh.traffic_class = 0;
+    attr.ah_attr.is_global = 1;
+    attr.ah_attr.port_num = 1;
+    memcpy(&attr.ah_attr.grh.dgid, dgid, 16);
+    attr.ah_attr.grh.flow_label = 0;
+    attr.ah_attr.grh.hop_limit = 1;
+    attr.ah_attr.grh.sgid_index = 2;
+    attr.ah_attr.grh.traffic_class = 0;
 
     flags = IBV_QP_STATE | IBV_QP_AV | IBV_QP_PATH_MTU | IBV_QP_DEST_QPN | IBV_QP_RQ_PSN | IBV_QP_MAX_DEST_RD_ATOMIC | IBV_QP_MIN_RNR_TIMER;
     return ibv_modify_qp(qp, &attr, flags);
@@ -381,7 +381,7 @@ static void connect_qpair(rdma_context_t* context)
     printf("|--modify_qp_to_init = %d\n", ret);
 
     // modify the QP to RTR
-    ret = modify_qp_to_rtr(context->qp[0], remote_qp_info->qp_num, remote_qp_info->lid, remote_qp_info->gid);
+    ret = modify_qp_to_rtr(context->qp[0], remote_qp_info->qp_num, remote_qp_info->lid, "0xb8cef6030089bda2");
     printf("|--modify_qp_to_rtr = %d\n", ret);
 
     // modify QP state to RTS
