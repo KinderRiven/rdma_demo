de6015abf5035666aa792ca7cbd0ecaa6a3cd4d6
KinderRiven <1339764596@qq.com>
2021-07-15 17:03:51 +0800

    rdma demo simple push

--- client.cc
+++ client.cc
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-06-17 10:56:52
- * @LastEditTime: 2021-07-15 17:02:42
+ * @LastEditTime: 2021-07-15 17:03:30
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /rdma_demo/hello_rdma.cc
@@ -403,8 +403,8 @@ static void connect_qpair(rdma_context_t* context)
     printf("|--modify_qp_to_init = %d\n", ret);
 
     // modify the QP to RTR
-    // ret = modify_qp_to_rtr(context->qp[0], remote_qp_info->qp_num, remote_qp_info->lid, (uint8_t*)(&remote_qp_info->gid));
-    // printf("|--modify_qp_to_rtr = %d\n", ret);
+    ret = modify_qp_to_rtr(context->qp[0], remote_qp_info->qp_num, remote_qp_info->lid, (uint8_t*)(&remote_qp_info->gid));
+    printf("|--modify_qp_to_rtr = %d\n", ret);
 
     // modify QP state to RTS
     ret = modify_qp_to_rts(context->qp[0]);
