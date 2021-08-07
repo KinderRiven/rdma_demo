b58010abaa689e5e8436341192a9cbdbef0f5942
KinderRiven <1339764596@qq.com>
2021-08-07 13:40:56 +0800

    rdma demo simple push

--- client.cc
+++ client.cc
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-06-17 10:56:52
- * @LastEditTime: 2021-08-07 13:39:03
+ * @LastEditTime: 2021-08-07 13:40:31
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /rdma_demo/hello_rdma.cc
@@ -465,9 +465,7 @@ static int post_receive(rdma_context_t* context)
 static void poll_cq(rdma_context_t* context)
 {
     int num_wc = 1;
-    struct ibv_qp** qp = context->qp;
     struct ibv_cq* cq = context->cq;
-    struct ibv_srq* srq = context->srq;
     struct ibv_wc wc;
     // wc = (struct ibv_wc*)calloc(num_wc, sizeof(struct ibv_wc));
 
